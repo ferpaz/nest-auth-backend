42354a50860b32604cacd120aae6e0bb2831ffaf
Fernando Paz <ferpaz64@gmail.com>
2024-08-08 15:07:43 -0600

    Configuración de la base y de la colección de usuarios

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+
 import { AuthController } from './auth.controller';
+import { MongooseModule } from  '@nestjs/mongoose';
+import { UserSchema } from './entities/user.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'User', schema: UserSchema }
+    ]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,29 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema()
+export class User {
+    // _id: number;   <-- mongo va a crear un id por defecto con este nombre
+
+    @Prop( { required: true, unique: true } )
+    email: string;
+
+    @Prop( { required: true } )
+    name: string;
+
+    @Prop( { required: true, minlength: 6 } )
+    password: string;
+
+    @Prop( { default: true } )
+    isActive: boolean;
+
+    @Prop({ type: [String], default: ['user'] })
+    role: string[];
+
+    @Prop( { default: Date.now } )
+    createdAt: Date;
+
+    @Prop( { default: null } )
+    updatedAt?: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);

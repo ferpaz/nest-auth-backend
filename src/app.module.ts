367aa8c81801b5d1823133a6f1b6e7a5b362e7c8
Fernando Paz <ferpaz64@gmail.com>
2024-08-08 14:50:35 -0600

    Se creo el esqueleto del controlador, servicio, DTOs y entidades para el API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule
+  ],
   controllers: [],
   providers: [],
 })
